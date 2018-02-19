86bbad46cc1b585c4b28620471174945e9fc9795
Duanger <aaronshenglu@gmail.com>
2018-02-19 16:42:32 -0500

    Added the commuter colliding text popup mechanic

---
+++ Assets/Scripts/CommuterCollision.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommuterCollision : MonoBehaviour
+{
+
+	public GameObject PopUpCanvas;
+
+	//private Transform playerTransform;
+	private CameraShake camShakey;
+    private Transform playerRotation;
+	// Use this for initialization
+	void Start ()
+	{
+		//playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+		camShakey = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraShake>();
+		playerRotation = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Transform>();
+	}
+
+	private void OnCollisionEnter(Collision other)
+	{
+		
+		if (other.collider.CompareTag("Player"))
+		{
+			GameObject popUp = Instantiate(PopUpCanvas, transform.position,Quaternion.Euler(new Vector3(0f,playerRotation.rotation.y,0f)));
+			camShakey.SubwayShaking = true;
+		}
+	}
+}
---
+++ Assets/Scripts/PopUpBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopUpBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	private CanvasRenderer popUpCanvasRenderer;
+	private Text popUpImage;
+	private Transform playerLookTransform;
+	void Start ()
+	{
+		popUpImage = GetComponent<Text>();
+		popUpCanvasRenderer = GetComponent<CanvasRenderer>();
+		playerLookTransform = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Transform>();
+		Vector3 playerDirection = playerLookTransform.position - transform.position;
+		playerDirection.x = 0;
+		playerDirection.z = 0;
+		transform.LookAt(playerLookTransform.position - playerDirection);
+		transform.Rotate(0,180,0);
+		StartCoroutine(PopUpFade());
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		transform.position += transform.up * 0.01f;
+	}
+
+	IEnumerator PopUpFade()
+	{
+		popUpImage.CrossFadeAlpha(0,3,false);
+		if (popUpCanvasRenderer.GetAlpha() == 0)
+		{
+			Destroy(gameObject);
+		}
+		yield return null;
+	}
+}
