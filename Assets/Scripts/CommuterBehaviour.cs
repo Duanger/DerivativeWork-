355665b1b3ac614a2c974f0b1b205efdeaec26cd
Lu HuangdiDESKTOP <aaronshenglu@gmail.com>
2018-02-19 00:55:51 -0500

    Added color lerping function to protoype commuters

---
+++ Assets/Scripts/CommuterBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommuterBehaviour : MonoBehaviour
+{
+
+	public float SittingDuration;
+	public Color DefaultColor;
+	public Color TargetColor;
+	
+	private float currentTime;
+	private Renderer communterRenderer;
+	private Rigidbody communterRigid;
+		
+	void Start ()
+	{
+		communterRenderer = GetComponent<Renderer>();
+		communterRigid = GetComponent<Rigidbody>();
+		communterRenderer.material.color = DefaultColor;
+	}
+	
+	void FixedUpdate ()
+	{
+		currentTime += Time.deltaTime;
+		StartCoroutine(CommuterColoring(SittingDuration));
+	}
+
+	IEnumerator CommuterColoring(float sitDuration)
+	{
+		communterRenderer.material.color = Color.Lerp(DefaultColor, TargetColor, currentTime / sitDuration);
+		if (communterRenderer.material.color == TargetColor)
+		{
+			communterRigid.useGravity = true;
+		}
+		yield return null;
+		StopCoroutine(CommuterColoring(sitDuration));
+	}
+}
