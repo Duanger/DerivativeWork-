ddd836dc348d9d801e3910e376b974db73b1ca32
Duanger <aaronshenglu@gmail.com>
2018-02-21 20:51:55 -0500

    started on navmesh

--- Assets/Scripts/CommuterBehaviour.cs
+++ Assets/Scripts/CommuterBehaviour.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class CommuterBehaviour : MonoBehaviour
 {
@@ -12,20 +13,28 @@ public class CommuterBehaviour : MonoBehaviour
 	private float currentTime;
 	private Renderer communterRenderer;
 	private Rigidbody communterRigid;
-		
+	private NavMeshAgent commuterNavMeshAgent;
 	void Start ()
 	{
 		communterRenderer = GetComponent<Renderer>();
 		communterRigid = GetComponent<Rigidbody>();
+		commuterNavMeshAgent = GetComponent<NavMeshAgent>();
 		communterRenderer.material.color = DefaultColor;
+		GoTo();
 	}
 	
 	void FixedUpdate ()
 	{
+		
 		currentTime += Time.deltaTime;
-		StartCoroutine(CommuterColoring(SittingDuration));
+		//StartCoroutine(CommuterColoring(SittingDuration));
 	}
 
+	void GoTo()
+	{
+		Vector3 targetVector3 = new Vector3(0.194908f,0f,4.103632f);
+		commuterNavMeshAgent.SetDestination(targetVector3);
+	}
 	IEnumerator CommuterColoring(float sitDuration)
 	{
 		communterRenderer.material.color = Color.Lerp(DefaultColor, TargetColor, currentTime / sitDuration);
